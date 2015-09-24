181fd864a64c0a96e7c41cadec280d23cfc80421
andrewdbass <andybass12@gmail.com>
2015-09-24 16:16:25 -0600

    Fix migrations again

--- db/migrate/20150921235729_sorcery_core.rb
+++ db/migrate/20150921235729_sorcery_core.rb
@@ -3,7 +3,7 @@ class SorceryCore < ActiveRecord::Migration
     create_table :users do |t|
       t.string :email,            :null => false
       t.string :crypted_password
-      t.string :salt]
+      t.string :salt
       t.integer :person_id
 
       t.timestamps
